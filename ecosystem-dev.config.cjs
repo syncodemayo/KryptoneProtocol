4bb7275ceaea1561d9749d585d21ab4dc83962c4
syncodemayo <zeroedtruth@proton.me>
2026-01-29 20:55:50 +0500

    Updated Pipeline and PM2 Config

--- ecosystem-dev.config.cjs
+++ ecosystem-dev.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'privacyescrow-dev',
+      name: 'kryptoneprotocol-dev',
       script: 'api/api.js',
       instances: 1,
       exec_mode: 'cluster',
